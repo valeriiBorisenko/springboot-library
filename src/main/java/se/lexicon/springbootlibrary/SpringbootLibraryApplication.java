5f18a933bc0047fab14035bf3437e748450bf527
Valerii Borisenko <valerii.borisenko@gmail.com>
2025-01-16 09:51:35 +0100

    init app

---
+++ src/main/java/se/lexicon/springbootlibrary/SpringbootLibraryApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.springbootlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootLibraryApplication.class, args);
+	}
+
+}
