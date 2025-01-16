22421b3838dcc63683f143f8043e8b545d89b6bb
Valerii Borisenko <valerii.borisenko@gmail.com>
2025-01-16 10:23:19 +0100

    define AppUser and Details

--- src/main/java/se/lexicon/springbootlibrary/entity/AppUser.java
+++ src/main/java/se/lexicon/springbootlibrary/entity/AppUser.java
@@ -1,17 +1,102 @@
 package se.lexicon.springbootlibrary.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 public class AppUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private String userName;
+
+    @Column(unique = true)
+    private String username;
     private String password;
     private LocalDate regDate;
 
     @OneToOne
     private Details userDetails;
+
+    protected AppUser() {}
+
+    public AppUser(int id, String username, String password, LocalDate regDate, Details userDetails) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.regDate = regDate;
+        this.userDetails = userDetails;
+    }
+
+    public AppUser(String username, String password, LocalDate regDate, Details userDetails) {
+        this.username = username;
+        this.password = password;
+        this.regDate = regDate;
+        this.userDetails = userDetails;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(LocalDate regDate) {
+        this.regDate = regDate;
+    }
+
+    public Details getUserDetails() {
+        return userDetails;
+    }
+
+    public void setUserDetails(Details userDetails) {
+        this.userDetails = userDetails;
+    }
+
+    @Override
+    public String toString() {
+        return "AppUser{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", regDate=" + regDate +
+                ", userDetails=" + userDetails +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        AppUser appUser = (AppUser) o;
+        return id == appUser.id && Objects.equals(username, appUser.username) && Objects.equals(password, appUser.password) && Objects.equals(regDate, appUser.regDate) && Objects.equals(userDetails, appUser.userDetails);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, password, regDate, userDetails);
+    }
 }
--- src/main/java/se/lexicon/springbootlibrary/entity/Details.java
+++ src/main/java/se/lexicon/springbootlibrary/entity/Details.java
@@ -1,14 +1,88 @@
 package se.lexicon.springbootlibrary.entity;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 public class Details {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(unique = true)
     private String email;
     private String name;
     private LocalDate birthDate;
 
+    protected Details() {}
+
+    public Details(int id, String email, String name, LocalDate birthDate) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public Details(LocalDate birthDate, String name, String email) {
+        this.birthDate = birthDate;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Details{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Details details = (Details) o;
+        return id == details.id && Objects.equals(email, details.email) && Objects.equals(name, details.name) && Objects.equals(birthDate, details.birthDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, email, name, birthDate);
+    }
 }
