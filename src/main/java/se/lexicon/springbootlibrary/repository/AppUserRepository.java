feef3b4f472ca46d4bf7dbbacc7cec0fce0efb6c
Valerii Borisenko <valerii.borisenko@gmail.com>
2025-01-16 11:12:36 +0100

    part2: update AppUserRepository and DetailsRepository

--- src/main/java/se/lexicon/springbootlibrary/repository/AppUserRepository.java
+++ src/main/java/se/lexicon/springbootlibrary/repository/AppUserRepository.java
@@ -1,7 +1,17 @@
 package se.lexicon.springbootlibrary.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import se.lexicon.springbootlibrary.entity.AppUser;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface AppUserRepository extends CrudRepository<AppUser, Integer> {
+    AppUser findUserByUsername(String username);
+    List<AppUser> findUsersByRegDateBetween(LocalDate startDate, LocalDate endDate);
+    AppUser findUserByUserDetailsId(Integer id);
+    AppUser findUserByUserDetailsEmailIgnoreCase(String email);
+    @Query("select u from  AppUser u  where u.userDetails.email= :email ")
+    List<AppUser> findUsersDetailsEmail(String userDetailsEmail);
 }
--- src/main/java/se/lexicon/springbootlibrary/repository/DetailsRepository.java
+++ src/main/java/se/lexicon/springbootlibrary/repository/DetailsRepository.java
@@ -3,5 +3,12 @@ package se.lexicon.springbootlibrary.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import se.lexicon.springbootlibrary.entity.Details;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface DetailsRepository extends JpaRepository<Details, Integer> {
+    List<Details> findDetailsByEmail(String email);
+    List<Details> findDetailsByNameContains(String name);
+    List<Details> findDetailsByNameIgnoreCase(String name);
+    List<Details> findDetailsByBirthDate(LocalDate birthday);
 }
