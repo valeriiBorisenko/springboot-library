4dc4f92fedfaffe663282c60558040ca8ee5c133
Valerii Borisenko <valerii.borisenko@gmail.com>
2025-01-23 11:30:54 +0100

    part5: delete comment

--- src/main/java/se/lexicon/springbootlibrary/repository/AuthorRepository.java
+++ src/main/java/se/lexicon/springbootlibrary/repository/AuthorRepository.java
@@ -14,7 +14,6 @@ public interface AuthorRepository extends CrudRepository<Author, Integer> {
     List<Author> findByLastName(String lastName);
     @Query( "SELECT a FROM Author a WHERE a.firstName LIKE %:name% OR a.lastName LIKE %:name%")
     List<Author> findByFirstNameOrLastNameContains(@Param("name") String name);
-    //@Query("SELECT a FROM Author a JOIN FETCH a.writtenBooks b WHERE b.id = :bookId")
     List<Author> findByWrittenBooksId(int bookId);
     @Query("UPDATE Author a SET a.firstName = :firstName, a.lastName = :lastName WHERE a.id = :author_id")
     void updateAuthorsNameById(@Param("author_id") Integer authorId, @Param("firstName") String firstName, @Param("lastName") String lastName);
